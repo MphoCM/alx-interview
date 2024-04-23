11e1ac9a7b93eb60a155b76b498d58d76d2cf202
MphoCM <christian.mofokeng@gmail.com>
2024-04-23 16:26:28 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,38 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
+/**
+ * Fetches and prints the names of actors in a Star Wars movie.
+ * @param {string} movieId - The ID of the Star Wars movie.
+ */
+const printActors = (movieId) => {
+  // Making a request to the Star Wars API to get information about the movie
+  request('https://swapi-api.hbtn.io/api/films/' + movieId, function (err, res, body) {
+    if (err) throw err;
+    // Parsing the response body to extract the list of actors in the movie
+    const actors = JSON.parse(body).characters;
+    // Calling the function to print actors in the exact order
+    exactOrder(actors, 0);
+  });
+};
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+/**
+ * Recursively fetches and prints the names of actors in exact order.
+ * @param {string[]} actors - An array of URLs representing the actors in the movie.
+ * @param {number} x - The current index in the actors array.
+ */
+const exactOrder = (actors, x) => {
+  // Base case: if x reaches the end of the actors array, return
+  if (x === actors.length) return;
+  // Making a request to get information about the actor at the current index
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    // Parsing the response body to extract the name of the actor
+    console.log(JSON.parse(body).name);
+    // Calling exactOrder recursively to process the next actor
+    exactOrder(actors, x + 1);
+  });
+};
 
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-});
+// Calling the printActors function with the movie ID provided as a command-line argument
+printActors(process.argv[2]);
